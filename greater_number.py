211f8a1fe57e1b4ef802510d0713060913ecda82
ibenito <73246581+benitoexe@users.noreply.github.com>
2024-11-27 14:07:58 -0700

    Add files via upload

--- greater_number.py
+++ greater_number.py
@@ -4,6 +4,7 @@ def find_max(a,b):
         return a
     else:
         return b
+print(find_max(11, 4))
 
 
 #no fuction code
@@ -11,5 +12,7 @@ number1 = input("enter a number:  ")
 number2 = input("enter another number:   ")
 if number1 > number2:
     print(f"{number1} is greater")
-else:
+if number2 > number1:
     print(f"{number2} is greater")
+else:
+    print(f"{number1} and {number2} are equal")
